fd8aab3a652823115e63433af93dd5df66c6418c
bluesberyyajm1966 <149900026+bluesberyyajm1966@users.noreply.github.com>
2023-12-06 21:06:26 -0800

    Update and rename index.html to hangman.js

---
+++ hangman.js
@@ -0,0 +1,99 @@
+const basketballWords = ['dribble', 'hoop', 'slam', 'shoot', 'block', 'rebound', 'jumpshot', 'freethrow', 'court', 'jersey'];
+let selectedWord = '';
+let guessedWord = [];
+let wrongLetters = [];
+let attempts = 6;
+
+function initializeGame() {
+    selectedWord = getRandomWord(basketballWords);
+    guessedWord = Array(selectedWord.length).fill('_');
+    wrongLetters = [];
+    attempts = 6;
+
+    displayWord();
+    updateAttempts();
+    updateWrongLetters();
+    updateHangmanVisual();
+}
+
+function getRandomWord(wordList) {
+    const randomIndex = Math.floor(Math.random() * wordList.length);
+    return wordList[randomIndex];
+}
+
+function displayWord() {
+    const wordDisplay = document.getElementById('word-display');
+    wordDisplay.textContent = guessedWord.join(' ');
+}
+
+function updateAttempts() {
+    document.getElementById('attempts').textContent = attempts;
+}
+
+function updateWrongLetters() {
+    const wrongLettersDisplay = document.getElementById('wrong-letters');
+    wrongLettersDisplay.textContent = wrongLetters.length > 0 ? `Wrong Letters: ${wrongLetters.join(', ')}` : '';
+}
+
+function updateHangmanVisual() {
+    const hangmanVisual = document.getElementById('hangman-visual');
+    hangmanVisual.innerHTML = '';
+
+    const parts = 6 - attempts; // Number of parts to draw
+    for (let i = 0; i < parts; i++) {
+        const part = document.createElement('div');
+        part.className = 'hangman-part';
+        hangmanVisual.appendChild(part);
+    }
+}
+
+function makeGuess() {
+    const letterInput = document.getElementById('letter-input');
+    const guessedLetter = letterInput.value.toLowerCase();
+
+    if (!guessedLetter.match(/[a-z]/i)) {
+        alert('Please enter a valid letter.');
+        return;
+    }
+
+    if (guessedWord.includes(guessedLetter) || wrongLetters.includes(guessedLetter)) {
+        alert('You already guessed this letter.');
+        return;
+    }
+
+    if (selectedWord.includes(guessedLetter)) {
+        updateGuessedWord(guessedLetter);
+        if (!guessedWord.includes('_')) {
+            endGame(true);
+        }
+    } else {
+        wrongLetters.push(guessedLetter);
+        attempts--;
+        updateWrongLetters();
+        updateAttempts();
+        if (attempts === 0) {
+            endGame(false);
+        }
+        updateHangmanVisual();
+    }
+
+    letterInput.value = '';
+}
+
+function updateGuessedWord(letter) {
+    for (let i = 0; i < selectedWord.length; i++) {
+        if (selectedWord[i] === letter) {
+            guessedWord[i] = letter;
+        }
+    }
+    displayWord();
+}
+
+function endGame(isWinner) {
+    const message = isWinner ? 'Congratulations! You guessed the word.' : `Sorry, you ran out of attempts. The word was "${selectedWord}".`;
+    alert(message);
+    initializeGame();
+}
+
+// Initialize the game when the page loads
+document.addEventListener('DOMContentLoaded', initializeGame);
