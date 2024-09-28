b2a11200614cc3e8dd8bfae2613b9a511fe2ddb3
bluesberyyajm1966 <149900026+bluesberyyajm1966@users.noreply.github.com>
2024-09-27 21:30:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Smooth Scrolling for Navigation Links
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
 
